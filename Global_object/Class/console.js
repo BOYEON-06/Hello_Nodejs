81079d67cf365aba67b92a5b0c13ab4f26f20582
BOYEON-06 <61862512+BOYEON-06@users.noreply.github.com>
2021-08-03 13:41:11 +0900

    Add Global_object

---
+++ Global_object/Class/console.js
@@ -0,0 +1,5 @@
+// console.log() : 콘솔에 로그 메세지 출력
+console.log('%d + %d = %d', 1, 2, 1+2);
+// console.time(label) : 시간 측정 시작 & console.timeEnd(label) : 시간 측정 종료
+console.time('time');
+console.timeEnd('time');
---
+++ Global_object/ExportsObj/main.js
@@ -0,0 +1,3 @@
+var caculator = require('./test.js');
+console.log('Double value is '+ caculator.double(3));
+console.log('Plus value is ' + caculator.plus(3));
